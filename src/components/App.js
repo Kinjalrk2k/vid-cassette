b4dcb377f026c993783db62fe57ae47c06ec4523
Kinjal Raykarmakar <kinjalrk2k@gmail.com>
2021-02-25 20:24:17 +0530

    video fetching done

--- src/components/App.js
+++ src/components/App.js
@@ -2,17 +2,35 @@ import React from "react";
 import SearchBar from "./SearchBar";
 import { Container, AppBar, Typography, Toolbar } from "@material-ui/core";
 
+import youtube from "../apis/youtube";
+
 class App extends React.Component {
+  state = { videos: [] };
+
+  onTermSubmit = async (term) => {
+    const response = await youtube.get("/search", {
+      params: {
+        part: "snippet",
+        maxResults: 5,
+        type: "video",
+        key: process.env.REACT_APP_YOUTUBE_API,
+        q: term,
+      },
+    });
+
+    this.setState({ videos: response.data.items });
+  };
+
   render() {
     return (
       <div>
         <AppBar position="static">
           <Toolbar>
             <Typography variant="h6">vidCasseTTe</Typography>
-            <SearchBar />
+            <SearchBar onFormSubmit={this.onTermSubmit} />
           </Toolbar>
         </AppBar>
-        <Container>Hello World!</Container>
+        <Container>{this.state.videos.length} videos found!</Container>
       </div>
     );
   }
